beae9962dc1fdecbf6546d42839c3db1b735fc10
Abdul Kaiyum <ma.kaiyum1992@gmail.com>
2018-02-05 16:43:46 +0600

    Update connect.php

--- database_connect/connect.php
+++ database_connect/connect.php
@@ -1,8 +1,8 @@
 <?php
 
 $host="abdulkaiyum.com";
-$user="kaiyum123";
-$pass="X42e77HHOmikAH";
-$db="kaiyum12_evoting";
+$user="userName";
+$pass="passWord";
+$db="DBname";
 $connect=  mysql_connect($host,$user,$pass) or die(mysql_error()."Your Data base is not connected");
 $selectDb=  mysql_select_db($db,$connect) or die(mysql_error()."Your Database is not selected");
