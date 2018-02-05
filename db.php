9ec0933a2d74ece704e6b3ce4404e1346b23bad3
Abdul Kaiyum <ma.kaiyum1992@gmail.com>
2018-02-05 16:50:16 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php 
-$conn=mysql_connect('abdulkaiyum.com','kaiyum123','X42e77HHOmikAH');
-$db=mysql_select_db('kaiyum12_evoting',$conn);
+$conn=mysql_connect('abdulkaiyum.com','userName','passWord');
+$db=mysql_select_db('DBname',$conn);
 ?>
